892ce2aa1718928dc30c8bded27acc60ab27b1c8
Andrey Sautenko <sautenkoandrey@gmail.com>
2024-07-01 18:17:32 +0300

    removed global variable in userelearns mode and add a new feature called RELOADER there. First tests showed good results. It need more test.

--- Learn_Words_App_0_0_1/userlearns.h
+++ Learn_Words_App_0_0_1/userlearns.h
@@ -35,6 +35,8 @@ private slots:
 
     void modeChanged();
 
+    void on_reloadDataButton_clicked();
+
 private:
     Ui::UserLearns *ui;
 
@@ -61,64 +63,10 @@ private:
     void DrawLabel(QString &&path, QLabel *label);
 
     void waitingMovie();
+
+    void prepareData(const QString &request_msg,
+                     const QString &path_to_flag,
+                     QSqlQuery &query);
 };
-
-
-
-// // a new approach
-// class UserLearns : public QDialog, public Observer
-// {
-//     Q_OBJECT
-
-// public:
-//     explicit UserLearns(ChangeLessonMode *changeLessonMode, QWidget *parent = nullptr);
-
-//     ~UserLearns();
-
-//     virtual void update() override;
-
-// private slots:
-//     void on_statsButton_clicked();
-
-//     void on_restartButton_clicked();
-
-//     void on_finishButton_clicked();
-
-//     void on_showtasksButton_clicked();
-
-//     void on_nextButton_clicked();
-
-// private:
-//     Ui::UserLearns *ui;
-
-//     OpenDB db;
-
-//     QHash<QString, QString> all_words;
-
-//     unsigned counter = 0;
-
-//     unsigned right_answers = 0;
-
-//     unsigned answers_counter = 0;
-
-//     double progress_steps = 1.0;
-
-//     void answer_is_right(const QString &task, const QString &answer) noexcept;
-
-//     void display_first_word();
-
-//     double get_stats() noexcept;
-
-//     void save_stats();
-
-//     void DrawLabel(QString &&path, QLabel *label);
-
-//     void waitingMovie();
-
-//     All_Languges m_languageMode = All_Languges::ENG;
-//     //All_Languges m_languageMode;
-
-//     ChangeLessonMode *m_changeLessonMode{nullptr};
-// };
 
 #endif // USERLEARNS_H
